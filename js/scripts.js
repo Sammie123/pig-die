2843909dd2800f01822ef1f7837e333975d3a5c5
lincoln nguyen and michael dunlap <lincoln+michael@>
2017-06-14 10:19:19 -0700

    Add player 2 and Add if-else statement if they rolled a 1

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ Die.prototype.rollDie = function() {
 
 var Player = function() {
   this.score = 0;
+  this.hasDie = false;
 }
 
 Player.prototype.updateScore = function (points) {
@@ -16,22 +17,44 @@ Player.prototype.updateScore = function (points) {
 }
 
 var die = new Die();
-var player = new Player();
+var player1 = new Player();
+var player2 = new Player();
 
-
-
-
+player1.hasDie = true;
 
 //interface
 $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var points = die.rollDie();
-    player.updateScore(points);
-    // alert(points);
-    // alert(player.score);
+    //var points = die.rollDie(); // roll die
+    //player.updateScore(points); // update score
+
+    if (player1.hasDie) {
+      var points = die.rollDie();
+      if (points === 1) {
+        player1.hasDie = false;
+        player2.hasDie = true;
+      } else {
+        player1.updateScore(points);
+      }
+    } else {
+      var points = die.rollDie();
+      if (points === 1) {
+        player2.hasDie = false;
+        player1.hasDie = true;
+      } else {
+        player2.updateScore(points);
+      }
+    }
+
     $(".roll").text(points);
-    $(".score").text(player.score);
+    //$(".score").text(player.score);
+
+    $(".playerScore1").text(player1.score);
+    $(".playerScore2").text(player2.score);
+
+
+
   });
 });
